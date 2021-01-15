62cfb15264b89c6d4b24195b917ea760ec069809
rawhite22 <robert.a.white625@gmail.com>
2021-01-14 23:28:52 -0500

    index.js changes

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const textInputs = document.querySelectorAll('input[type=text]');
 const checkInputs = document.querySelectorAll('input[type=checkbox]');
 const scoreOutPut = document.querySelector('.total');
 
-console.log(checkInputs);
-
-const calcPoints = (goals, assists, sog, bs, shp, shtBonus) => {
+const calcPoints = (
+  goals,
+  assists,
+  sog,
+  bs,
+  shp,
+  shtBonus,
+  htrBonus,
+  blkBonus,
+  pointBonus
+) => {
   let score = goals * 8.5 + assists * 5 + sog * 1.5 + bs * 1.3 + shp * 2;
   let bonuses = 0;
   shtBonus ? (bonuses += 3) : bonuses;
-  console.log(bonuses);
+  htrBonus ? (bonuses += 3) : bonuses;
+  blkBonus ? (bonuses += 3) : bonuses;
+  pointBonus ? (bonuses += 3) : bonuses;
+
   const total = score + bonuses;
   return total;
 };
@@ -20,7 +31,20 @@ const submit = () => {
   const blksht = +textInputs[3].value;
   const shp = +textInputs[4].value;
   const shtBonus = checkInputs[0].checked;
-  console.log(shtBonus);
-  const score = calcPoints(goals, assists, sog, blksht, shp, shtBonus);
+  const htrBonus = checkInputs[1].checked;
+  const blkBonus = checkInputs[2].checked;
+  const pntBonus = checkInputs[3].checked;
+
+  const score = calcPoints(
+    goals,
+    assists,
+    sog,
+    blksht,
+    shp,
+    shtBonus,
+    htrBonus,
+    blkBonus,
+    pntBonus
+  );
   console.log(score);
 };
